5d89415d1d885860bb0ad01f7b9884b0e922465a
ChuDucManh <chuducm2009@gmail.com>
2026-05-10 18:24:01 +0700

    Fixed dungeon overlay

--- src/main/java/io/github/moulberry/notenoughupdates/dungeons/DungeonBlocks.java
+++ src/main/java/io/github/moulberry/notenoughupdates/dungeons/DungeonBlocks.java
@@ -66,7 +66,7 @@ public class DungeonBlocks {
 
 	public static boolean isOverriding() {
 		return OpenGlHelper.isFramebufferEnabled() && NotEnoughUpdates.INSTANCE.config.dungeons.enableDungBlockOverlay &&
-			(NotEnoughUpdates.INSTANCE.config.dungeons.dungeonBlocksEverywhere ||
+			(NotEnoughUpdates.INSTANCE.config.dungeons.dungeonBlocksEverywhere || SBInfo.getInstance().isInDungeon ||
 				(SBInfo.getInstance().getLocation() != null && SBInfo.getInstance().getLocation().equals("dungeon")));
 	}
 
